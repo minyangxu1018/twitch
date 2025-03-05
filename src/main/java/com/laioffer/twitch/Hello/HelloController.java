9e9c944128342e254a44e660a742321b3ee5afa0
minyangxu1018 <mx2269@nyu.edu>
2025-03-05 00:03:01 -0500

    Adding development tester Updating main entrance Adding twitch api client

--- src/main/java/com/laioffer/twitch/Hello/HelloController.java
+++ src/main/java/com/laioffer/twitch/Hello/HelloController.java
@@ -4,6 +4,7 @@ package com.laioffer.twitch.Hello;
 import com.laioffer.twitch.Hello.Person;
 import net.datafaker.Faker;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 
@@ -12,15 +13,19 @@ public class HelloController {
 
 
     @GetMapping("/hello")
-    public Person sayHello() {
+    public Person sayHello(@RequestParam(required = false) String name) {
         Faker faker = new Faker();
-        String name = faker.name().fullName();
+        if (name == null) {
+            name = "Guest";
+        }
         String company = faker.company().name();
         String street = faker.address().streetAddress();
         String city = faker.address().city();
         String state = faker.address().state();
         String bookTitle = faker.book().title();
         String bookAuthor = faker.book().author();
+
+
         return new Person(name, company, new Address(street, city, state, null), new Book(bookTitle, bookAuthor));
     }
 }
